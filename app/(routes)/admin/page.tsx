d947e24cf904645bef255bdc424d89caa2938b2a
mohamadreza <mohamadrezamohammady2019@gmail.com>
2023-12-16 23:10:35 +0430

    admin page change

--- app/(routes)/admin/page.tsx
+++ app/(routes)/admin/page.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
+import React from "react";
 import Dashboard from "@/app/ui/admin/Dashboard";
-import {getServerSession} from "next-auth";
-import {authOptions} from "@/app/api/auth/[...nextauth]/route";
-import {redirect} from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+import AdminHomePage from "@/app/ui/admin/AdminHomePage";
 
 const AdminPage = async () => {
-    const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-    if (!session)
-        redirect("/login?callbackUrl=/admin")
-    return (
-       <></>
-    );
+  if (!session) redirect("/login?callbackUrl=/admin");
+  return <AdminHomePage />;
 };
 
 export default AdminPage;
